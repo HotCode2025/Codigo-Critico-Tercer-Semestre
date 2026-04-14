6747500337bf0399de6af3dfe58d27220c862166
Ariel Mazara <arielmazara@gmail.com>
2026-04-14 00:07:31 -0300

    Se agrega codigo con la incorporacion de texto al archivo txt Clase 2 Item 1.1 video 2. Closes #11

--- Python/Clase 2 Manejo de Archivos/manejo_archivos.py
+++ Python/Clase 2 Manejo de Archivos/manejo_archivos.py
@@ -1,6 +1,8 @@
 #Declaramos una variable
 try:
     archivo = open('Prueba.txt', 'w') # La W es de WRITE que significa ESCRIBIR
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally: #siempre se ejecuta
