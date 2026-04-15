132d926f50266e14de7ec4a8f40100a66e9c9a5b
Ariel Mazara <arielmazara@gmail.com>
2026-04-15 16:20:40 -0300

    Se hace Clase 2 Punto 1.2 Video 3

--- Python/Clase 2 Manejo de Archivos/manejo_archivos.py
+++ Python/Clase 2 Manejo de Archivos/manejo_archivos.py
@@ -1,9 +1,13 @@
 #Declaramos una variable
 try:
-    archivo = open('Prueba.txt', 'w') # La W es de WRITE que significa ESCRIBIR
+    archivo = open('Prueba.txt', 'w', encoding='utf8') # La W es de WRITE que significa ESCRIBIR
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo: acción, ejecución y producción\n')
     archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally: #siempre se ejecuta
     archivo.close() # Con esto se debe cerrar el archivo.
+
+# archivo.write('Todo quedo perfecto') Este es un error
