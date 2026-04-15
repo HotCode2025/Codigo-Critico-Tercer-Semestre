b4d0143077f3f2c511b826ee5623e0a1d69e1f85
Agustina Zuniga <agustinavictoriazuniga@gmail.com>
2026-04-15 19:43:02 -0300

    Se hace Clase 2 Punto 2.2. Modo StrictVideo 2. Closes #22

--- JavaScript/Clase 02 Modo Strict y Sobreescritura/02-ModoStrict.js
+++ JavaScript/Clase 02 Modo Strict y Sobreescritura/02-ModoStrict.js
@@ -1,13 +1,14 @@
 "use Strict";  //se usa al inicio del programa
  
-const x = 10;
+let x = 10;
 console.log(x)
 
-miFuncion();
 
 function miFuncion(){
-   // "use strict" No permite el uso de ninguna variable sin declarar
+   "use strict"  //No permite el uso de ninguna variable sin declarar
     y = 10; 
     console.log(y)
 }
-miFuncion();
+miFuncion(); 
+
+//Comentario se debe reiniciar quokka cuando se usa modo strict para que ejecute bien el modo strict y sepa que la variable esta definida.
