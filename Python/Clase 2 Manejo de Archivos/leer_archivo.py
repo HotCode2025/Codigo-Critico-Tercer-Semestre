2e06dc460dec52c531fdd6478e7cd71e5fe79df4
Ariel Mazara <arielmazara@gmail.com>
2026-04-15 17:10:22 -0300

    Se hace Clase 2 Punto 1.3 Video 7. Closes #19

--- Python/Clase 2 Manejo de Archivos/leer_archivo.py
+++ Python/Clase 2 Manejo de Archivos/leer_archivo.py
@@ -1,6 +1,7 @@
 
 archivo = open('Prueba.txt', 'r', encoding='utf8') # Las letras son: 'r' read, 'a' append, 'w' write, 'x'
-# print(archivo.read())
+#Si ela archivo txt lo tengo en otra carpeta debo especificar la direccion en Linux Mint es: /home/ariel/Documentos/Tecnicatura/Codigo-Critico-Tercer-Semestre/Python/Clase 2 Manejo de Archivos/Prueba.txt
+#print(archivo.read())
 #print(archivo.read(16))
 #print(archivo.read(10)) # Continuamos desde la linea anterior
 print(archivo.readline())
