ce33debef1558364001e8a6e6111ddf04da7f2af
Ariel Mazara <arielmazara@gmail.com>
2026-04-15 17:02:04 -0300

    Se hace Clase 2 Punto 1.3 Video 6. Closes #18

--- Python/Clase 2 Manejo de Archivos/leer_archivo.py
+++ Python/Clase 2 Manejo de Archivos/leer_archivo.py
@@ -1,4 +1,8 @@
 
 archivo = open('Prueba.txt', 'r', encoding='utf8') # Las letras son: 'r' read, 'a' append, 'w' write, 'x'
 # print(archivo.read())
+#print(archivo.read(16))
+#print(archivo.read(10)) # Continuamos desde la linea anterior
+print(archivo.readline())
+print(archivo.readline())
 
