85ebc31e5f3458a5b94fdb5c98dc563fbb5be26f
Ariel Mazara <arielmazara@gmail.com>
2026-04-16 18:14:01 -0300

    Finalizando Punto 1.4 Manejo de bloques de codigo Java. Closes #27

---
+++ Java/Clase 02 Bloques/BloquesInicializacionTeoria/src/test/TestBloqueInicializacionTeoria.java
@@ -0,0 +1,13 @@
+package test;
+
+import domainteoria.PersonaTeoria;
+        
+        public class TestBloqueInicializacionTeoria {
+    public static void main(String[] args) {
+        PersonaTeoria persona1 = new PersonaTeoria();
+        System.out.println("persona1 = " + persona1);
+        PersonaTeoria persona2 = new PersonaTeoria();
+        System.out.println("persona2 = " + persona2);
+                
+    }
+}
