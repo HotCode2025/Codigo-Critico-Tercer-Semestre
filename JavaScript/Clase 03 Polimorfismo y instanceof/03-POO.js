6972ac10c2b37ef509316718a55037894e05690d
Ariel Mazara <arielmazara@gmail.com>
2026-04-21 12:00:22 -0300

    Se hace Clase 03 Punto 3.2 instanceof JavaScript. Closes #37

--- JavaScript/Clase 03 Polimorfismo y instanceof/03-POO.js
+++ JavaScript/Clase 03 Polimorfismo y instanceof/03-POO.js
@@ -23,8 +23,18 @@ class Gerente extends Empleado{
 }
 
 //POLIFORMISMO UNA SOLA LINEA DE CODIGA EN EL METODO IMPIRMIR EJECUTA EL METODO DE LA CLASE PADRE O HIJA. MULTIPLES FORMAS EN TIEMPOS DE EJECUCION
-function imprimir( tipo ){
-    console.log(tipo.obtenerDetalles() );
+function imprimir( tipo ){ // Recibe una variable
+    console.log(tipo.obtenerDetalles() ); // Segun el tipo que le pasemos, será la información.
+    if(tipo instanceof Gerente){
+        console.log('Es un objeto de tipo Gerente');
+        console.log(tipo._departamento) //No existe en la clase padre. 
+    }
+    // else if (tipo instanceof Empleado){
+    //     console.log('Es de tipo Empleado');
+    // }
+    else if(tipo instanceof Object){ //El orden siempre es jerarquico
+        console.log('Es de tipo Object'); // Clase padre de todas las clases
+    }
 }
 
 let gerente1 = new Gerente("Carlos", 5000, "sistemas");
@@ -33,5 +43,7 @@ console.log(gerente1); //Objeto de la clase hija
 let empleado1 = new Empleado("Juan", 3000);
 console.log(empleado1); //Objeto de la clase padre
 
-imprimir(gerente1);
-imprimir(empleado1);
+imprimir(gerente1); //En el poliformismo esta llamando al método de la clase padre
+imprimir(empleado1); // Esta llamando al método de la clase hija
+
+//Son las multiples formas en tiempo de ejecucion del poliformismo.
