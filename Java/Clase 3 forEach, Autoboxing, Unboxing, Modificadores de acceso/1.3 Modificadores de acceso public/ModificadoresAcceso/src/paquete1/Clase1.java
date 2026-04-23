94a17c6d64874a81c96274371ea31a92de68d9fc
Ariel Mazara <arielmazara@gmail.com>
2026-04-23 17:56:50 -0300

    Se hace Clase 3 Punto 1.4 Modificacores de Acceso PROTECTED de JAVA video 7. Closes #42

--- Java/Clase 3 forEach, Autoboxing, Unboxing, Modificadores de acceso/1.3 Modificadores de acceso public/ModificadoresAcceso/src/paquete1/Clase1.java
+++ Java/Clase 3 forEach, Autoboxing, Unboxing, Modificadores de acceso/1.3 Modificadores de acceso public/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -1,7 +1,10 @@
 package paquete1;
 
+// private class Clase 1 ESTO NO EXISTE
 public class Clase1 {
     public String atributoPublic = "Valor atributo public";
+    
+    protected String atributoProtected = "Valor Atributo Protected";
     
     
     public Clase1(){
@@ -9,8 +12,16 @@ public class Clase1 {
     }
     
     
+    protected Clase1(String atributoPublic) {
+            System.out.println("Constructor protected");
+    }
+    
     public void metodoPublico(){
         System.out.println("Metodo public");
+    }
+    
+    protected void metodoProtected (){
+        System.out.println("Metodo Protected");
     }
             
        
