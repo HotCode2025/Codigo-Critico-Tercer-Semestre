209ebbc9993d3380580f4fa2609e35564d6a2eaa
Ariel Mazara <arielmazara@gmail.com>
2026-05-05 11:26:52 -0300

    Se hace CLASE 5 PYTHON 5.2 Funcion fetchone en psycopg2. Closes #70

--- Python/Clase 5 Registro Postgresql y Python/prueba_bd.py
+++ Python/Clase 5 Registro Postgresql y Python/prueba_bd.py
@@ -12,9 +12,15 @@ conexion = psycopg2.connect(
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia ='SELECT * FROM persona'
-            cursor.execute(sentencia) #De esta manera ejecutamos la sentencia
-            registros = cursor.fetchall() # Recuperamos todos los registros que serán una lista
+            # sentencia ='SELECT * FROM persona' # Quito asterisco ya que eso signidica todo
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s'  # %s PLACEHOLDER Usar solo un registro filtro
+            # sentencia = 'SELECT id_persona, nombre From Persona' solo seleccino ahi id y persona
+
+            id_persona = input('Digite un número para el id_persona: ') #defino el parametro y el placeholder busca valor
+            cursor.execute(sentencia, (id_persona,)) #se hace en tuplas la eleccion de valores. La tupla lleva siempre coma ,
+            # cursor.execute(sentencia) #De esta manera ejecutamos la sentencia
+            #registros = cursor.fetchall() # Recuperamos todos los registros que serán una lista
+            registros = cursor.fetchone() #nos muestra directamente la tupla y desaparece la lista
             print(registros)
 except Exception as e:
     print(f'Ocurrio un error: {e}')
