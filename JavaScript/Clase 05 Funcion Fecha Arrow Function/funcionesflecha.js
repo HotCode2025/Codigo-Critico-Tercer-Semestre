da1ada7a72c086f69dcafad58e4a07db31b7411b
Ariel Mazara <arielmazara@gmail.com>
2026-05-06 17:10:47 -0300

    Se hace CLASE 5 JAVASCRIPT Funcion Flecha Arrow Function 5.1 Ejemplos Clasicos. Closes #79

---
+++ JavaScript/Clase 05 Funcion Fecha Arrow Function/funcionesflecha.js
@@ -0,0 +1,23 @@
+
+
+miFuncion();
+
+function miFuncion(){
+    console.log('Saludar desde mi funcion')
+}
+
+miFuncion();
+
+let myFuncion = function() {
+    console.log('Saludos desde la funcion anonima')
+}
+
+
+// Ahora vamos a crear una funcion flecha
+
+let miFuncionFlecha = () => {
+    console.log('Saludos desde mi funcion flecha');
+}
+
+// Hay mas variantes para funciones flecha que vamos a ir viendo
+miFuncionFlecha();
