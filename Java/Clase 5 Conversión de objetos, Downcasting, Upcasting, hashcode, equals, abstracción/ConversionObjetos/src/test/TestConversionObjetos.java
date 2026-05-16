1ec4c6ad0a8a21f91bc5df39b00086ea1af02561
Ariel Mazara <arielmazara@gmail.com>
2026-05-16 12:21:39 -0300

    Se hace clase 5 JAVA Completa. Closed #83 Closes #84 Closes #85 Closes #86

--- Java/Clase 5 Conversión de objetos, Downcasting, Upcasting, hashcode, equals, abstracción/ConversionObjetos/src/test/TestConversionObjetos.java
+++ Java/Clase 5 Conversión de objetos, Downcasting, Upcasting, hashcode, equals, abstracción/ConversionObjetos/src/test/TestConversionObjetos.java
@@ -1,6 +1,6 @@
 package test;
 
-import domain.*;
+import domain.*; // * hereda todas las clases del paquete domain
 
 public class TestConversionObjetos {
     public static void main(String[] args) {
@@ -12,13 +12,13 @@ public class TestConversionObjetos {
         
         //empleado.getTipoEscritura(); NO SE PUEDE HACER
         
-        //Downcasting
+        //Downcasting convertimos un tipo padre a la clase hija
         //((Escritor)empleado).getTipoEscritura(); //Tenemos 2 opciones: esta es una
         Escritor escritor = (Escritor)empleado; //Esta es la segunda opción
         escritor.getTipoEscritura();
         
-        //Upcasting
-        //Empleado empleado2 = escritor;
-        //System.out.println(empleado2.obtenerDetalles());
+        //Upcasting tipo de la clase hija a un tipo de la clase padre
+        Empleado empleado2 = escritor;
+        System.out.println(empleado2.obtenerDetalles());
     }
 }
