4790c7eea38ed8af971a8d1eb823278feb612636
Joel Gonzalez <joelious7@gmail.com>
2026-06-02 21:59:38 -0300

    Clase 8 - 9.1 método seleccionar en PersonaDao. Closes #117

---
+++ Python/Clase 8 Capa de Datos -  Logging y Postgresql Parte 3/capa_datos_persona/persona_dao.py
@@ -0,0 +1,29 @@
+from conexion import Conexion
+from persona import Persona
+
+class PersonaDAO:
+    """
+    DAO significa: Data Access Object
+    CRUD significa:
+                    Create -> Insertar
+                    Read -> Seleccionar
+                    Update -> Actualizar
+                    Delete -> Eliminar
+    """
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+    _ACTUALIZAR ='UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE  id_persona=%s'
+
+    # Definimos los métodos de clase
+
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []
+                for registro in registros:
+                    persona = Persona(registro[0], registro[2], registro[3])
+                    personas.append(persona)
