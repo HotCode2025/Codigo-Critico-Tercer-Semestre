381702ef32e0a9e63f83e50f9a592b667b5ec067
Joel Gonzalez <joelious7@gmail.com>
2026-06-03 19:57:25 -0300

    Clase 8 - 9.5 metodo eliminar en PersonaDao. Closes #121

--- Python/Clase 8 Capa de Datos -  Logging y Postgresql Parte 3/capa_datos_persona/persona_dao.py
+++ Python/Clase 8 Capa de Datos -  Logging y Postgresql Parte 3/capa_datos_persona/persona_dao.py
@@ -47,16 +47,33 @@ class PersonaDAO:
                 log.debug(f'Persona actualizada: {persona}')
                 return cursor.rowcount
 
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Los objetos eliinados son: {persona}')
+                return cursor.rowcount
+
+
 if __name__ == '__main__':
+    #Eliminar un registro
+    persona1 = Persona(id_persona=5)
+    persona_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {persona_eliminadas}')
+
+
     # Actualizar un registro
-    persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
-    personas_actualizadas = PersonaDAO.actualizar(persona1)
-    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    #persona1 = Persona(1, 'Juan José', 'Pena', 'jjpena@mail.com')
+    #personas_actualizadas = PersonaDAO.actualizar(persona1)
+    #log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
     # Insertar un registro
     #persona1 = Persona(nombre='Omero', apellido='Ramos', email='omeror@gmail.com')
     #personas_insertadas = PersonaDAO.insertar(persona1)
     #log.debug(f'Personas insertadas: {personas_insertadas}')
+
     # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
     for persona in personas:
