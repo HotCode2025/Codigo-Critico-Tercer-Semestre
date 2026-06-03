182fdeed0390db88685dc6a2290828601ed9de5d
Ariel Mazara <arielmazara@gmail.com>
2026-06-02 23:36:42 -0300

    Se hace Clase 8 Punto 8.4 Creamos nuestra propia excepcion parte 1,2 y3 Video 7-8-9. Closes #127

--- Java/Clase 8 Excepciones en Java/ManejoExcepciones1/src/test/TestExcepciones.java
+++ Java/Clase 8 Excepciones en Java/ManejoExcepciones1/src/test/TestExcepciones.java
@@ -1,16 +1,19 @@
 package test;
 
+import static aritmetica.Aritmetica.division;
+
 
 public class TestExcepciones { // el codigo o nuestro programa sigue corriendo a pesar de un error por eso son excepciones
     public static void main(String[] args) {
        int resultado = 0;
-       // try{
-                resultado = 10/0;
-//       }  catch(Exception e){
-//           System.out.println("Ocurrio un Error");
-//           e.printStackTrace(System.out); //Se conoce como la pila de excepciones
-//           
-//       }
+        try{
+                resultado = division(10, 0);
+       }  catch(Exception e){
+           System.out.println("Ocurrio un Error");
+           e.printStackTrace(System.out); //Se conoce como la pila de excepciones
+            System.out.println(e.getMessage());
+           
+       }
         System.out.println("La Variable de Resultado tiene como valor: "+resultado);
                     
     }
