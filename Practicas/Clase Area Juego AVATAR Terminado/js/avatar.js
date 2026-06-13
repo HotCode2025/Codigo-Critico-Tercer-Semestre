9544b53141eddb59c797bb94ea582e4c40074356
Ariel Mazara <arielmazara@gmail.com>
2026-06-13 17:15:28 -0300

    Se agrega audio y mejora interface

--- Practicas/Clase Area Juego AVATAR Terminado/js/avatar.js
+++ Practicas/Clase Area Juego AVATAR Terminado/js/avatar.js
@@ -1,5 +1,10 @@
 let vidasJ = 3, vidasE = 3, pNameJ = "", pNameE = "";
 
+function toggleMusica() {
+    let musica = document.getElementById('musica-fondo');
+    musica.paused ? musica.play() : musica.pause();
+}
+
 function seleccionarPersonaje(nombre, clase) {
     pNameJ = nombre;
     document.getElementById('selection-screen').style.display = 'none';
@@ -7,12 +12,15 @@ function seleccionarPersonaje(nombre, clase) {
     document.getElementById('action-footer').style.display = 'flex';
     document.getElementById('player-card').className = 'card card-bg ' + clase;
     
+    // Música automática al iniciar batalla
+    let musica = document.getElementById('musica-fondo');
+    musica.play().catch(e => console.log("Esperando interacción..."));
+    
     const pjs = [
         {n: 'Aang', c: 'bg-aang'}, {n: 'Katara', c: 'bg-katara'}, 
         {n: 'Zuko', c: 'bg-zuko'}, {n: 'Toph', c: 'bg-toph'}
     ];
     
-    // Filtrado para que la IA elija un enemigo distinto
     const pjsDisponibles = pjs.filter(p => p.n !== nombre);
     let enemigo = pjsDisponibles[Math.floor(Math.random() * pjsDisponibles.length)];
     
@@ -33,12 +41,11 @@ function atacar(tipo) {
     let color = (res === "GANASTE") ? "var(--win)" : (res === "PERDISTE") ? "var(--lose)" : "var(--gold)";
     let panel = document.getElementById('battle-history');
     panel.innerHTML += `<p style="color:${color}">${pNameJ}(${tipo}) vs ${pNameE}(${e}): <strong>${res}</strong></p>`;
-    panel.scrollTop = panel.scrollHeight; // Scroll automático al final
+    panel.scrollTop = panel.scrollHeight;
     
     if(vidasJ === 0 || vidasE === 0) {
         document.getElementById('btn-reiniciar').style.visibility = 'visible';
         document.querySelectorAll('.action-card').forEach(b => b.disabled = true);
-        
         let mensaje = (vidasJ === 0) ? "¡DERROTA! HAS SIDO VENCIDO" : "¡VICTORIA! ERES EL MAESTRO AVATAR";
         let colorFin = (vidasJ === 0) ? "var(--lose)" : "var(--win)";
         panel.innerHTML += `<p style="text-align:center; font-size:1.3rem; font-weight:bold; color:${colorFin}; margin-top:20px; border-top:2px solid ${colorFin}; padding-top:10px;">${mensaje}</p>`;
