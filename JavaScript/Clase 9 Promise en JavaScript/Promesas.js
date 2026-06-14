84d42d99759dd7b4081288c7e15ab0115c25d3ce
Agustina Zuniga <agustinavictoriazuniga@gmail.com>
2026-06-13 23:02:16 -0300

    Se hace clase 9 JS Promesas Punto 10.4 Funcion setTimeout y Promesas, arreglando un error.Closes #163

--- JavaScript/Clase 9 Promise en JavaScript/Promesas.js
+++ JavaScript/Clase 9 Promise en JavaScript/Promesas.js
@@ -1,5 +1,5 @@
 let miPromesa = new Promise((resolver, rechazar) => {
-    let exprpersion = false;
+    let exprpersion = true;
     if(exprpersion){
         resolver('Resolvió correctamente');
     } else {
@@ -12,6 +12,15 @@ let miPromesa = new Promise((resolver, rechazar) => {
 //    error => console.log(error)
 //);
 
-miPromesa
-    .then( valor => console.log(valor))
-    .catch(error => console.log(error));
+//miPromesa
+//   .then( valor => console.log(valor))
+//    .catch(error => console.log(error));
+
+let promesa = new Promise ((resolver) => {
+    console.log('Inicio Promesa');
+    setTimeout( () => resolver('Saludos desde promesa, callback, funcion flecha y setTimeout'),3000);
+    console.log('Final Promesa');
+});
+
+//El llamado a la promesa
+promesa.then(valor => console.log(valor));
